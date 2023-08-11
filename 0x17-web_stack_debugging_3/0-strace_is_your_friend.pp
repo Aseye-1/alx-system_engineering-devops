bdfa6ee3b37ef523fb084367dbcde1b129e7d456
Aseye-1 <aboraseye@gmail.com>
2023-08-11 02:22:43 +0000

    updated

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
-# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
-
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+# Fourth web-stack debug script in puppet
+exec { 'wsd3':
+  command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/' /var/www/html/wp-settings.php",
+  path    =>  ['/bin', '/usr/bin']
 }
